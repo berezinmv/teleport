5487077edefc81c7c612f83b73a70c772ba92d49
berezinmv <berezin.mvdev@yahoo.com>
2016-05-28 15:14:19 +0500

    Add 'auth' module

---
+++ app/auth.js
@@ -0,0 +1,110 @@
+var users = require("./users");
+var jwt = require("jsonwebtoken");
+var express = require("express");
+var router = express.Router();
+
+router.use(function (req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, "secret", function (err, decoded) {
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
+router.post("/register", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (!username || !password) {
+    return res.json({
+      success: false,
+      message: "Invalid username or password"
+    });
+  }
+
+  var newUser = {username: username, password: password};
+  users.findOne(newUser, function (err, user) {
+    if (err) {
+      return res.json({
+        success: false,
+        message: "Error occurred",
+        error: err
+      });
+    }
+    if (user) {
+      return res.json({
+        success: false,
+        message: "User already exist"
+      });
+    }
+    users.insert(newUser, function (err, user) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: "Error occurred",
+          error: err
+        });
+      }
+      var token = jwt.sign(user, "secret", {expiresIn: "24h"});
+      res.json({
+        success: true,
+        token: token
+      });
+    })
+  });
+});
+
+router.post("/authenticate", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (!username || !password) {
+    return res.json({
+      success: false,
+      message: "Invalid username or password"
+    });
+  }
+
+  users.findOne({username: username, password: password}, function (err, user) {
+    if (err) {
+      return res.json({
+        success: false,
+        message: "Error occurred",
+        error: err
+      });
+    }
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User doesn't exist"
+      });
+    }
+    var token = jwt.sign(user, "secret", {expiresIn: "24h"});
+    res.json({
+      success: true,
+      token: token
+    });
+  });
+});
+
+router.get("/user", function (req, res) {
+  users.findOne(req.decoded, function (err, user) {
+    if (err) {
+      return res.json({
+        success: false,
+        message: "Error occurred",
+        error: err
+      });
+    }
+    res.json({
+      success: true,
+      user: user
+    });
+  });
+});
+
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -5,9 +5,11 @@ var server = http.createServer(app);
 
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(express.static("public", {index: "index.html"}));
+
+app.use(require("./auth"));
 
 module.exports = server;
